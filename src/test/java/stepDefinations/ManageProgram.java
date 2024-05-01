4664d7e90a785a850ae434dabe7f13137e76d9fe
Kanchan Basudkar <kanchan.basudkar@gmail.com>
2024-04-30 20:17:29 -0400

    My changes committed

--- src/test/java/stepDefinations/ManageProgram.java
+++ src/test/java/stepDefinations/ManageProgram.java
@@ -15,13 +15,14 @@ import static utility.TestBase.driver;
 //import utility.TestSetupManager;
 
 public class ManageProgram extends C_ProgramPOM {
-PageObject.A_LoginPOM a_loginPOM = new PageObject.A_LoginPOM(driver);
-     //TestSetupManager testSetupManager;
+    PageObject.A_LoginPOM a_loginPOM = new PageObject.A_LoginPOM(driver);
+
+    //TestSetupManager testSetupManager;
 //    C_ProgramPOM c_programPOM;
-     public ManageProgram() {
-         this.driver = driver;
-         PageFactory.initElements(driver, this);
-     }
+    public ManageProgram() {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
 
     private reusableMethods reusablemethod = new reusableMethods(driver);
 
@@ -37,7 +38,7 @@ PageObject.A_LoginPOM a_loginPOM = new PageObject.A_LoginPOM(driver);
 //        this.testSetupManager= testSetupManager;
 //    }
 
-//    @Given("Logged on the LMS portal as Admin")
+    //    @Given("Logged on the LMS portal as Admin")
 //    public void logged_on_the_lms_portal_as_admin() {
 //        reusablemethod.explicitWait(pageObjectManager.getlogin().username);
 //
@@ -136,7 +137,7 @@ PageObject.A_LoginPOM a_loginPOM = new PageObject.A_LoginPOM(driver);
 
     @Given("Admin is on dashboard page after Login and clicks Program on the navigation bar")
     public void admin_is_on_dashboard_page_after_login_and_clicks_program_on_the_navigation_bar() {
-             program.click();
+        program.click();
 
     }
 
@@ -156,5 +157,101 @@ PageObject.A_LoginPOM a_loginPOM = new PageObject.A_LoginPOM(driver);
         System.out.println("my test ");
         Assert.assertTrue(isSaveAndCancelVisibleInAddProgramPopup());
     }
+
+    @Given("Admin is on Manage Program Page after clicks Program on the navigation bar")
+    public void admin_is_on_manage_program_page_after_clicks_program_on_the_navigation_bar() {
+
+
+    }
+
+    @When("Admin clicks Edit button on the data table for any row")
+    public void admin_clicks_edit_button_on_the_data_table_for_any_row() {
+
+
+    }
+
+    @Then("Admin should see a popup open for Program details to edit")
+    public void admin_should_see_a_popup_open_for_program_details_to_edit() {
+
+
+    }
+
+    @Given("Admin is on Program Details Popup window to Edit")
+    public void admin_is_on_program_details_popup_window_to_edit() {
+
+
+    }
+
+    @When("Admin edits the Name column and clicks save button")
+    public void admin_edits_the_name_column_and_clicks_save_button() {
+
+
+    }
+
+    @Then("Admin gets a message {string} alert and able to see the updated name in the table for the particular program")
+    public void admin_gets_a_message_alert_and_able_to_see_the_updated_name_in_the_table_for_the_particular_program(String string) {
+
+
+    }
+
+    @When("Admin edits the Description column and clicks save button")
+    public void admin_edits_the_description_column_and_clicks_save_button() {
+
+
+    }
+
+    @Then("Admin gets a message {string} alert and able to see the updated description in the table for the particular program")
+    public void admin_gets_a_message_alert_and_able_to_see_the_updated_description_in_the_table_for_the_particular_program(String string) {
+
+
+    }
+
+    @When("Admin changes the Status and clicks save button")
+    public void admin_changes_the_status_and_clicks_save_button() {
+
+
+    }
+
+    @Then("Admin gets a message {string} alert and able to see the updated status in the table for the particular program")
+    public void admin_gets_a_message_alert_and_able_to_see_the_updated_status_in_the_table_for_the_particular_program(String string) {
+
+
+    }
+
+    @When("Admin enters only numbers or special char in name and desc column")
+    public void admin_enters_only_numbers_or_special_char_in_name_and_desc_column() {
+
+
+    }
+
+    @Then("Admin gets a Error message alert")
+    public void admin_gets_a_error_message_alert() {
+
+
+    }
+
+    @When("Admin clicks Cancel button on edit popup")
+    public void admin_clicks_cancel_button_on_edit_popup() {
+
+
+    }
+
+    @Then("Admin can see the Program details popup disappears and can see nothing changed for particular program")
+    public void admin_can_see_the_program_details_popup_disappears_and_can_see_nothing_changed_for_particular_program() {
+
+
+    }
+
+    @When("Admin clicks Save button on edit popup")
+    public void admin_clicks_save_button_on_edit_popup() {
+
+
+    }
+
+    @Then("Admin gets a message Successful Program Updated alert and able to see the updated details in the table for the particular program")
+    public void admin_gets_a_message_successful_program_updated_alert_and_able_to_see_the_updated_details_in_the_table_for_the_particular_program() {
+
+    }
+
 
 }
