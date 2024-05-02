d80c082653a6c5094707ad12e336e2eb32024b36
Kanchan Basudkar <kanchan.basudkar@gmail.com>
2024-05-02 00:32:24 -0400

    My changes committed

--- src/test/java/PageObject/PageObjectManager.java
+++ src/test/java/PageObject/PageObjectManager.java
@@ -1,22 +1,22 @@
 //package PageObject;
 //
 //import org.openqa.selenium.WebDriver;
-///
+//
 //public class PageObjectManager {
 //
-//	
+//
 //	public A_LoginPOM Login;
 //	public WebDriver driver;
-//	
+//
 //	public PageObjectManager(WebDriver driver) {
 //		this.driver= driver;
 //	}
 //
 //	public A_LoginPOM getlogin() {
-//		
+//
 //		Login= new A_LoginPOM(driver);
-//		
+//
 //		return Login;
 //	}
-//	
+//
 //}
